b33152095c4f3985b3b6347e9193b781ed67d0cb
Martin Wilhelmi <mnin@mnin.de>
2010-08-02 11:32:04 +0200

    remove debug puts

--- lib/action_controller/instrumentation/xmlrpc_endpoint.rb
+++ lib/action_controller/instrumentation/xmlrpc_endpoint.rb
@@ -26,7 +26,6 @@ module ActionController
         # TODO: add route automatically for this?
         def xe_index
           result = @xmlrpc_server.process(request.body)
-          puts "\n\n----- BEGIN RESULT -----\n#{result}----- END RESULT -----\n\n"
           render :text => result, :content_type => 'text/xml'
         end
 
@@ -37,7 +36,6 @@ module ActionController
           # loop through all the methods, adding them as handlers
           self.class.instance_methods(false).each do |method|
             unless ['xe_index', 'xe_method_prefix'].member?(method)
-              puts "Adding XMLRPC method for #{method.to_s}"
               @xmlrpc_server.add_handler(xe_method_prefix + method) do |*args|
                 self.send(method.to_sym, *args)
               end
